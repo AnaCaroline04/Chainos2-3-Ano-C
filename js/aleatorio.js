6b081cab31ab343b890f130aa79283df758f4bf1
AnaCaroline04 <134461085+AnaCaroline04@users.noreply.github.com>
2024-08-27 07:46:01 -0300

    Add files via upload

---
+++ js/aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Ana Caroline","Yasmim","Duda","Naju","Cryslane"];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
